1b306ea4fa0de12581f3880c3a9f466be89615a7
itsleg <its_leg@proton.me>
2026-09-03 22:06:42 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = "workout-v11";
+const ASSETS = ["./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install", (e) => { e.waitUntil(caches.open(CACHE).then((c) => Promise.allSettled(ASSETS.map((a) => c.add(a)))).then(() => self.skipWaiting())); });
+self.addEventListener("activate", (e) => { e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener("fetch", (e) => { const req = e.request; if (req.method !== "GET") return; e.respondWith(caches.match(req).then((cached) => cached || fetch(req).then((resp) => { const copy = resp.clone(); caches.open(CACHE).then((c) => c.put(req, copy)).catch(() => {}); return resp; }).catch(() => (req.mode === "navigate" ? caches.match("./index.html") : undefined)))); });
